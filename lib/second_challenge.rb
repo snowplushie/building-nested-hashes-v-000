53b4f084167ecf66d6c41287fd59248f03f5c8a2
Paige Wiener <snowplushie@yahoo.com>
2019-03-07 04:19:05 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,10 @@ def second_challenge
       :hero_friend => {}
       }, 
     :capulet => {
-      
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friend => {}
       }}
 
   
