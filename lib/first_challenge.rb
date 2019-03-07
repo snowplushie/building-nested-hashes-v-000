a0b8f58af6273011189381d4c6da95f6f77a713e
Paige Wiener <snowplushie@yahoo.com>
2019-03-07 04:17:16 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,11 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {},
+       :matriarch => {},
+       :hero => {},
+       :hero_friend => {}
+     },
+     :capulet => {}
+   }
 end
